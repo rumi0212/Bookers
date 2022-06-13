adcf3ca88586c4fc37601df7116c23a38f5e9b0f
rumi0212 <106750897+rumi0212@users.noreply.github.com>
2022-06-13 14:59:16 +0000

    [Add]bookのindexページのレイアウト作成,bookの新規登録機能の作成,bookのcreate機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,14 @@
 class BooksController < ApplicationController
+
   def new
+    @book = Book.new
   end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/show'
+　end
 
   def index
     @books = Book.all
@@ -11,4 +19,10 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+ end
 end

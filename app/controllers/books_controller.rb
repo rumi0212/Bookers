1fa507ae2208aef53aaf642aa2d756afd00bd9af
rumi0212 <106750897+rumi0212@users.noreply.github.com>
2022-06-17 15:07:02 +0000

    [Add]bookのindexページの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,11 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to '/show'
+    if book.save
+      redirect_to book_path(book.id)
+    else
+      render '/show'
+    end
 　end
 
   def index
@@ -15,6 +18,7 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
@@ -24,5 +28,5 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
- end
+end
 end

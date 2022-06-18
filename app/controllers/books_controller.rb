3f0524ba46beac4b1d217838c3d0229faf45ed04
rumi0212 <106750897+rumi0212@users.noreply.github.com>
2022-06-18 13:47:14 +0000

    [Add]bookのeditページの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,6 +26,15 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: 'Book was successfully updated.'
+    else
+      render :edit
+    end
+  end
 
   private
   def book_params
